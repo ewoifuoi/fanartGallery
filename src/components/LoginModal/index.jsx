3afc997f265fff1e0f32012bf251aa434fad1ba3
ewoifuoi <hamimelons@126.com>
2024-05-11 18:19:55 +0800

    update

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -4,7 +4,7 @@ import { Form, Button, Modal } from 'react-bootstrap';
 import './LoginModal.css'
 
 import './assets/css/styles.css'
-import './assets/img/img-login.svg'
+
 import './assets/js/main.js'
 
 const LoginModal = (props) => {
@@ -36,64 +36,64 @@ const LoginModal = (props) => {
                     </Form>
                     <Button variant="primary">登录</Button> */}
 
-                    <div class="login">
-                        <div class="login__content">
-                            <div class="login__img">
-                                <img src="assets/img/img-login.svg" alt="" />
+                    <div className="login">
+                        <div className="login__content">
+                            <div className="login__img">
+                                <img src="/images/img-login.svg" alt="" />
                             </div>
-                            <div class="login__forms">
-                                <form action="" class="login__registre" id="login-in">
-                                    <h1 class="login__title">Sign In</h1>
+                            <div className="login__forms">
+                                <form action="" className="login__registre" id="login-in">
+                                    <h1 className="login__title">Sign In</h1>
 
-                                    <div class="login__box">
-                                        <i class='bx bx-user login__icon'></i>
-                                        <input type="text" placeholder="Username" class="login__input" />
+                                    <div className="login__box">
+                                        <i className='bx bx-user login__icon'></i>
+                                        <input type="text" placeholder="Username" className="login__input" />
                                     </div>
 
-                                    <div class="login__box">
-                                        <i class='bx bx-lock-alt login__icon'></i>
-                                        <input type="password" placeholder="Password" class="login__input" />
+                                    <div className="login__box">
+                                        <i className='bx bx-lock-alt login__icon'></i>
+                                        <input type="password" placeholder="Password" className="login__input" />
                                     </div>
 
-                                    <a href="#" class="login__forgot">Forgot password?</a>
+                                    <a href="#" className="login__forgot">Forgot password?</a>
 
-                                    <a href="#" class="login__button">Sign In</a>
+                                    <a href="#" className="login__button">Sign In</a>
 
                                     <div>
-                                        <span class="login__account">Don't have an Account ?</span>
-                                        <span class="login__signin" id="sign-up">Sign Up</span>
+                                        <span className="login__account">Don't have an Account ?</span>
+                                        <span className="login__signin" id="sign-up">Sign Up</span>
                                     </div>
                                 </form>
 
-                                <form action="" class="login__create none" id="login-up">
-                                    <h1 class="login__title">Create Account</h1>
+                                <form action="" className="login__create none" id="login-up">
+                                    <h1 className="login__title">Create Account</h1>
 
-                                    <div class="login__box">
-                                        <i class='bx bx-user login__icon'></i>
-                                        <input type="text" placeholder="Username" class="login__input" />
+                                    <div className="login__box">
+                                        <i className='bx bx-user login__icon'></i>
+                                        <input type="text" placeholder="Username" className="login__input" />
                                     </div>
 
-                                    <div class="login__box">
-                                        <i class='bx bx-at login__icon'></i>
-                                        <input type="text" placeholder="Email" class="login__input" />
+                                    <div className="login__box">
+                                        <i className='bx bx-at login__icon'></i>
+                                        <input type="text" placeholder="Email" className="login__input" />
                                     </div>
 
-                                    <div class="login__box">
-                                        <i class='bx bx-lock-alt login__icon'></i>
-                                        <input type="password" placeholder="Password" class="login__input" />
+                                    <div className="login__box">
+                                        <i className='bx bx-lock-alt login__icon'></i>
+                                        <input type="password" placeholder="Password" className="login__input" />
                                     </div>
 
-                                    <a href="#" class="login__button">Sign Up</a>
+                                    <a href="#" className="login__button">Sign Up</a>
 
                                     <div>
-                                        <span class="login__account">Already have an Account ?</span>
-                                        <span class="login__signup" id="sign-in">Sign In</span>
+                                        <span className="login__account">Already have an Account ?</span>
+                                        <span className="login__signup" id="sign-in">Sign In</span>
                                     </div>
 
-                                    <div class="login__social">
-                                        <a href="#" class="login__social-icon"><i class='bx bxl-facebook' ></i></a>
-                                        <a href="#" class="login__social-icon"><i class='bx bxl-twitter' ></i></a>
-                                        <a href="#" class="login__social-icon"><i class='bx bxl-google' ></i></a>
+                                    <div className="login__social">
+                                        <a href="#" className="login__social-icon"><i className='bx bxl-facebook' ></i></a>
+                                        <a href="#" className="login__social-icon"><i className='bx bxl-twitter' ></i></a>
+                                        <a href="#" className="login__social-icon"><i className='bx bxl-google' ></i></a>
                                     </div>
                                 </form>
                             </div>
