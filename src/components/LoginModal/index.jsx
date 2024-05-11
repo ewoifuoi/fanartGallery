77f53b6b5da93c4e9bfbdfb2c1e1a3f350aac1b2
ewoifuoi <hamimelons@126.com>
2024-05-11 19:37:25 +0800

    实现点击空白处关闭模态框效果

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -8,6 +8,14 @@ import './assets/css/styles.css'
 import './assets/js/main.js'
 
 const LoginModal = (props) => {
+
+    // 处理点击模态框内容以外的区域关闭模态框
+    const handleModalBodyClick = (e) => {
+        // 检查是否点击了模态框内容以外的区域，并且不是 login__registre 元素
+        if (!e.target.closest('.login__registre')) {
+            props.onHide();
+        }
+    };
     return (
         <>
             <Modal
@@ -16,13 +24,11 @@ const LoginModal = (props) => {
                 size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
+
+                onHide={props.onHide}
             >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        登录
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+                
+                <Modal.Body onClick={handleModalBodyClick}>
                     {/* <Form>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>用户名</Form.Label>
@@ -35,7 +41,7 @@ const LoginModal = (props) => {
                         </Form.Group>
                     </Form>
                     <Button variant="primary">登录</Button> */}
-
+                    
                     <div className="login">
                         <div className="login__content">
                             <div className="login__img">
@@ -43,7 +49,7 @@ const LoginModal = (props) => {
                             </div>
                             <div className="login__forms">
                                 <form action="" className="login__registre" id="login-in">
-                                    <h1 className="login__title">Sign In</h1>
+                                    <h1 className="login__title">登录</h1>
 
                                     <div className="login__box">
                                         <i className='bx bx-user login__icon'></i>
@@ -57,16 +63,16 @@ const LoginModal = (props) => {
 
                                     <a href="#" className="login__forgot">Forgot password?</a>
 
-                                    <a href="#" className="login__button">Sign In</a>
+                                    <a href="#" className="login__button">登录</a>
 
                                     <div>
                                         <span className="login__account">Don't have an Account ?</span>
-                                        <span className="login__signin" id="sign-up">Sign Up</span>
+                                        <span className="login__signin" id="sign-up">注册</span>
                                     </div>
                                 </form>
 
                                 <form action="" className="login__create none" id="login-up">
-                                    <h1 className="login__title">Create Account</h1>
+                                    <h1 className="login__title">创建账号</h1>
 
                                     <div className="login__box">
                                         <i className='bx bx-user login__icon'></i>
@@ -83,11 +89,11 @@ const LoginModal = (props) => {
                                         <input type="password" placeholder="Password" className="login__input" />
                                     </div>
 
-                                    <a href="#" className="login__button">Sign Up</a>
+                                    <a href="#" className="login__button">注册</a>
 
                                     <div>
                                         <span className="login__account">Already have an Account ?</span>
-                                        <span className="login__signup" id="sign-in">Sign In</span>
+                                        <span className="login__signup" id="sign-in">登录</span>
                                     </div>
 
                                     <div className="login__social">
