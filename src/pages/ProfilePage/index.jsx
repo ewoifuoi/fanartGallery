fc538c8a7baa1d4cd62e574ce64c9d0478b8b7ec
ewoifuoi <hamimelons@126.com>
2024-05-24 22:11:33 +0800

    update

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
             <div className="p-2"></div>
             <div className="d-flex justify-content-center">
 
-                <div className="container_box" style={{ width: '96%', height: '1000px', minWidth:'1440px' }}>
+                <div className="container_box" style={{ width: '98%', height: '1000px', minWidth:'1440px' }}>
 
                     <div className="bg-warning background_img">
                         <img style={{ width: '100%' }} src="/images/default_background.jpg" alt="/images/default_background.jpg" />
@@ -66,6 +66,16 @@ const ProfilePage = () => {
 
                             </div>
                             <div className="d-flex" id="button_box">
+
+                                <div className="d-flex">
+                                    <div className="nav_title">作品</div>
+                                    <div className="nav_title">收藏</div>
+                                    <div className="nav_title">关注列表</div>
+                                    <div className="nav_title">粉丝列表</div>
+                                </div>
+
+                                <div style={{width:'550px'}}></div>
+
                                 <a className="custom-button">
                                     <div className="d-flex" style={{alignContent:'center', justifyContent:'center'}}>
                                         <svg t="1716553162486" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6543" width="23" height="23"><path d="M914.285714 73.142857h-804.571428C51.2 73.142857 0 124.342857 0 182.857143v585.142857c0 58.514286 51.2 109.714286 109.714286 109.714286h804.571428c58.514286 0 109.714286-51.2 109.714286-109.714286v-585.142857c0-58.514286-51.2-109.714286-109.714286-109.714286z m-804.571428 73.142857h804.571428L563.2 497.371429c-14.628571 14.628571-29.257143 21.942857-51.2 21.942857s-36.571429-7.314286-51.2-21.942857L109.714286 146.285714zM73.142857 782.628571V212.114286l285.257143 285.257143L73.142857 782.628571z m80.457143 21.942858l256-256c29.257143 29.257143 65.828571 43.885714 102.4 43.885714s73.142857-14.628571 102.4-43.885714l43.885714-43.885715-43.885714 43.885715 256 256H153.6z m797.257143-36.571429v14.628571L665.6 497.371429 950.857143 212.114286v555.885714z" p-id="6544" fill="#fff"></path></svg>
