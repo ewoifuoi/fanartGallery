a94779c2027692e9b97822c1cc1d1b80d801326d
ewoifuoi <hamimelons@126.com>
2024-06-05 04:04:24 +0800

    验证当前主页是否为用户本人所有

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -8,7 +8,6 @@ import Alerts from "../../components/Alerts";
 
 const ProfilePage = () => {
     
-    
     const navigate = useNavigate();
 
     const alertRef = useRef(null);
@@ -17,12 +16,15 @@ const ProfilePage = () => {
     let uid = params.uid
 
     const [username, setUsername] = useState('');
+    const userid = useSelector((state)=>state.auth.uid);
     const [email, setEmail] = useState('');
     const [workscount, setWorkscount] = useState('');
     const [followingcount, setFollowingcount] = useState('');
     const [followerscount, setFollowercount] = useState('')
     const [likecount, setLikecount] = useState('');
     const [avatarLink, setAvatarLink] = useState('/images/default.png')
+    const [owner, setOwner] = useState(true);
+    
 
     // 三级路由导航页标识
     // 0 : 作品列表
@@ -53,6 +55,14 @@ const ProfilePage = () => {
             const errorMessage = error.response ? error.response.data : '用户数据请求失败';
             alertRef.current.showAlert({ type: 'danger', msg: errorMessage });
         }
+        finally {
+            if(uid == userid) {
+                setOwner(true);
+            }
+            else {
+                setOwner(false);
+            }
+        }
     }
 
     const fetchAvatar = async () => {
@@ -71,9 +81,10 @@ const ProfilePage = () => {
       }
 
     useEffect(()=>{
+        if(!userid) return;
         fetchData();
         fetchAvatar();
-    },[])
+    },[userid])
 
     return (
         
@@ -156,21 +167,25 @@ const ProfilePage = () => {
 
                                 <div style={{width:'500px'}}></div>
 
-                                <a className="custom-button">
-                                    <div className="d-flex" style={{alignContent:'center', justifyContent:'center'}}>
-                                        <svg t="1716553162486" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6543" width="23" height="23"><path d="M914.285714 73.142857h-804.571428C51.2 73.142857 0 124.342857 0 182.857143v585.142857c0 58.514286 51.2 109.714286 109.714286 109.714286h804.571428c58.514286 0 109.714286-51.2 109.714286-109.714286v-585.142857c0-58.514286-51.2-109.714286-109.714286-109.714286z m-804.571428 73.142857h804.571428L563.2 497.371429c-14.628571 14.628571-29.257143 21.942857-51.2 21.942857s-36.571429-7.314286-51.2-21.942857L109.714286 146.285714zM73.142857 782.628571V212.114286l285.257143 285.257143L73.142857 782.628571z m80.457143 21.942858l256-256c29.257143 29.257143 65.828571 43.885714 102.4 43.885714s73.142857-14.628571 102.4-43.885714l43.885714-43.885715-43.885714 43.885715 256 256H153.6z m797.257143-36.571429v14.628571L665.6 497.371429 950.857143 212.114286v555.885714z" p-id="6544" fill="#fff"></path></svg>
-                                        <div style={{width:'9px'}}></div>
-                                        <div>私信</div>
+                                {!owner && (
+                                    <div className="d-flex">
+                                        <a className="custom-button">
+                                            <div className="d-flex" style={{alignContent:'center', justifyContent:'center'}}>
+                                                <svg t="1716553162486" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6543" width="23" height="23"><path d="M914.285714 73.142857h-804.571428C51.2 73.142857 0 124.342857 0 182.857143v585.142857c0 58.514286 51.2 109.714286 109.714286 109.714286h804.571428c58.514286 0 109.714286-51.2 109.714286-109.714286v-585.142857c0-58.514286-51.2-109.714286-109.714286-109.714286z m-804.571428 73.142857h804.571428L563.2 497.371429c-14.628571 14.628571-29.257143 21.942857-51.2 21.942857s-36.571429-7.314286-51.2-21.942857L109.714286 146.285714zM73.142857 782.628571V212.114286l285.257143 285.257143L73.142857 782.628571z m80.457143 21.942858l256-256c29.257143 29.257143 65.828571 43.885714 102.4 43.885714s73.142857-14.628571 102.4-43.885714l43.885714-43.885715-43.885714 43.885715 256 256H153.6z m797.257143-36.571429v14.628571L665.6 497.371429 950.857143 212.114286v555.885714z" p-id="6544" fill="#fff"></path></svg>
+                                                <div style={{width:'9px'}}></div>
+                                                <div>私信</div>
+                                            </div>
+                                        </a>
+                                    <div style={{width:'30px'}}></div>
+                                        <a className="custom-button">
+                                            <div className="d-flex" style={{alignContent:'center', justifyContent:'center'}}>
+                                                <svg t="1716552985086" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2592" width="26" height="23"><path d="M469.333333 469.333333V170.666667h85.333334v298.666666h298.666666v85.333334h-298.666666v298.666666h-85.333334v-298.666666H170.666667v-85.333334h298.666666z" fill="#fff" p-id="2593"></path></svg>
+                                                <div style={{width:'4px'}}></div>
+                                                <div>关注</div>
+                                            </div>
+                                        </a>
                                     </div>
-                                </a>
-                                <div style={{width:'30px'}}></div>
-                                <a className="custom-button">
-                                    <div className="d-flex" style={{alignContent:'center', justifyContent:'center'}}>
-                                        <svg t="1716552985086" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2592" width="26" height="23"><path d="M469.333333 469.333333V170.666667h85.333334v298.666666h298.666666v85.333334h-298.666666v298.666666h-85.333334v-298.666666H170.666667v-85.333334h298.666666z" fill="#fff" p-id="2593"></path></svg>
-                                        <div style={{width:'4px'}}></div>
-                                        <div>关注</div>
-                                    </div>
-                                </a>
+                                )}
                             </div>
                             <div className="p-5"></div>
                             <div className="p-5"></div>
