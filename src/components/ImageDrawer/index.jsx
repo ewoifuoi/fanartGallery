dab4676ce3a50f31cbf42fb2a4284bb913952d17
ewoifuoi <hamimelons@126.com>
2024-07-24 11:58:50 +0800

    update

--- src/components/ImageDrawer/index.jsx
+++ src/components/ImageDrawer/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import './ImageDrawer.css';
+import './imageDrawer.css';
 
 const ImageDrawer = ({ imageUrl,width }) => {
   const imageRef = useRef(null);
